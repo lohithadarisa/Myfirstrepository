6b82514f223126296bc3fea212ae154ea149d832
lohitha <lohithadarisa1@gmail.com>
2019-04-10 11:58:03 +0530

    added new class

---
+++ src/Newclass.java
@@ -0,0 +1,11 @@
+
+public class Newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("new class added for git practice");
+
+	}
+
+}
